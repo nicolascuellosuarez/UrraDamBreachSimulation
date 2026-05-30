b06e644cc8390f4e2fcc5b5e217413ca68f28286
Nicolás Cuello <jaimecuellosuarez@gmail.com>
2026-05-30 14:51:16 -0500

    Code of init conditions setted.

--- src/init.c
+++ src/init.c
@@ -30,7 +30,21 @@ void initialize_simulation_parameters(double *h, double *u, double *v, double *z
                 zb[idx] = 50.0 - slope * x + 2.0 * sin(0.001 * y);
             }
 
+            if (i < dam_x_pos) {
+                h[idx] = h_upstream - zb[idx];
+                if (h[idx] < 0) h[idx] = 0;
+            } else {
+                h[idx] = h_downstream - zb[idx];
+                if (h[idx] < 0) h[idx] = 0;
+            }
 
+            if (h[idx] < 0.01) h[idx] = 0.01; // Minimum Value of water level
         }
     }
+
+    printf("La inicialización de la Simulación fue completada:\n");
+    printf("Profundidad máxima aguas arriba: %.2d m\n", h_upstream);
+    printf("Profundidad aguas abajo: %.2d m\n", h_downstream);
+    printf("Posición de la presa: celda %d (%.0f m)\n", dam_x_pos, dam_x_pos * dx);
+    printf("Pendiente del Terreno: %.6f\n", slope);
 }
